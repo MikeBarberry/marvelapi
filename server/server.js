e6dc99e4bf49972dce58f2f7725b5f7196b9d1d5
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-02-27 19:26:03 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 
+// for heroku
+const path = require('path')
+
 const hero = require("./routes/api/hero")
 
 const app = express()
