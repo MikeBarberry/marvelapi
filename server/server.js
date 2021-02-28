29ae6de7f12634bd3029eece36f2ed782f36bcec
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-02-27 17:34:08 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv")
-dotenv.config( {path:'../.env'} ) 
+/* const dotenv = require("dotenv")
+dotenv.config( {path:'../.env'} )  */
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
