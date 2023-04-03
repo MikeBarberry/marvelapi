4d37bb5282926db59a19db086ffd03708d2a67c3
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2023-04-02 17:28:05 -0700

    add set-env config file

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  dev: {
+    awsRegion: 'us-west-2',
+    awsProfile: 'default',
+    ecrId: '095138843627',
+    ecrRepo: 'marvel-characters',
+  },
+  stage: {
+    awsRegion: 'us-west-2',
+    awsProfile: 'default',
+    ecrId: '095138843627',
+    ecrRepo: 'marvel-characters',
+  },
+  prod: {
+    awsRegion: 'us-west-2',
+    awsProfile: 'default',
+    ecrId: '095138843627',
+    ecrRepo: 'marvel-characters',
+  },
+};
